1a5b280d7f00f3ded9cdbd5c657152177819e8a6
Paz Marzana <pazmarzana@gmail.com>
2020-10-25 00:07:47 -0300

    javascript en hoja separada

--- control.js
+++ control.js
@@ -1,3 +1,45 @@
-function val(){
-    console.log("chequeando");
+function validarDatos(form){
+
+    if (validarUsuario(form))
+    {
+        if (validarClave(form)){
+            form.submit();
+        }
+    }
 }
+
+function validarUsuario(form) {
+const campo = form.usuario;
+    if (campo.value == "" ){
+        alert("Debe ingresar un usuario");
+        campo.focus();
+        return(false);
+    }else{
+        if (campo.value.indexOf('@',0) == -1){
+            alert("El usuario debe ser una direccion de email");
+            campo.focus();
+            return(false);
+        }else{
+            return(true);
+        }
+    }
+}
+
+function validarClave(form) {
+    const campo = form.clave;
+    if (campo.value == "" ){
+        alert("Debe ingresar una clave");
+        campo.focus();
+        return(false);
+    }else{
+        return(true);
+    }
+}
+
+document.getElementById("btnSubmit").addEventListener("click", function(event){
+    event.preventDefault();
+    const form=document.getElementById("form");
+    if(validarDatos(form)){
+        form.submit();
+    };
+});
